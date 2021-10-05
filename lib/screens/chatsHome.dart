bab4cd6923fe6be6938d574b1a9845597dac5995
Yuvraj Singh <ysgaur9919@gmail.com>
2021-10-05 21:21:27 +0530

    refactored widgets

--- lib/screens/chatsHome.dart
+++ lib/screens/chatsHome.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:musi_chat/constants/appHeading.dart';
-import 'package:musi_chat/constants/colors.dart';
-import 'package:musi_chat/provider/themeProvider.dart';
 import 'package:musi_chat/widgets/changeThemeButton.dart';
+import 'package:musi_chat/widgets/chatsHomeAppBar.dart';
 
 class ChatsHome extends StatefulWidget {
   @override
@@ -14,42 +11,9 @@ class _ChatsHomeState extends State<ChatsHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: SizedBox.shrink(),
-        title: Center(
-          child: AppHeading(),
-        ),
-        actions: [
-          Center(
-              child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(100),
-                  boxShadow: [
-                    BoxShadow(
-                        offset: Offset(0, 2),
-                        blurRadius: 5,
-                        color: Colors.black.withOpacity(0.3))
-                  ]),
-              child: CircleAvatar(
-                backgroundColor: Theme.of(context).accentColor,
-                child: Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: CircleAvatar(
-                    backgroundColor: Theme.of(context).primaryColor,
-                    child: Icon(
-                      Icons.settings_outlined,
-                      size: 25,
-                      color: Theme.of(context).shadowColor,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ))
-        ],
-      ),
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(kToolbarHeight),
+          child: ChatsHomeAppBar()),
       body: Center(
         child: ChangeThemeButtonWidget(),
       ),
---
+++ lib/widgets/chatsHomeAppBar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:musi_chat/constants/appHeading.dart';
+
+class ChatsHomeAppBar extends StatelessWidget {
+  const ChatsHomeAppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: SizedBox.shrink(),
+      title: Center(
+        child: AppHeading(),
+      ),
+      actions: [
+        Center(
+            child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(0, 2),
+                      blurRadius: 5,
+                      color: Colors.black.withOpacity(0.3))
+                ]),
+            child: CircleAvatar(
+              backgroundColor: Theme.of(context).accentColor,
+              child: Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: CircleAvatar(
+                  backgroundColor: Theme.of(context).primaryColor,
+                  child: Icon(
+                    Icons.settings_outlined,
+                    size: 25,
+                    color: Theme.of(context).shadowColor,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ))
+      ],
+    );
+  }
+}
