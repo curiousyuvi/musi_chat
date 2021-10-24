cc0db87d25af64a517c3c21bfc11dfdcf0f831bd
Yuvraj Singh <ysgaur9919@gmail.com>
2021-10-24 18:49:49 +0530

    changed chatScreenMessageTextField ui

--- lib/widgets/chatScreenMessageTextField.dart
+++ lib/widgets/chatScreenMessageTextField.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:line_icons/line_icons.dart';
 
 class ChatScreenMessageTextField extends StatelessWidget {
   bool isTyping;
@@ -20,56 +21,81 @@ class ChatScreenMessageTextField extends StatelessWidget {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
       width: double.infinity,
-      color: Theme.of(context).primaryColor,
-      child: Row(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Expanded(
-              child: Container(
-            decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
-                borderRadius: BorderRadius.circular(100),
-                border:
-                    Border.all(color: Theme.of(context).shadowColor, width: 1)),
-            padding: EdgeInsets.symmetric(horizontal: 18),
-            child: TextFormField(
-              onChanged: onChanged,
-              controller: controller,
-              cursorColor: Theme.of(context).shadowColor,
-              maxLines: 1,
-              textAlignVertical: TextAlignVertical.center,
-              style: Theme.of(context).textTheme.bodyText1,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(borderSide: BorderSide.none),
-                  contentPadding: EdgeInsets.zero,
-                  hintStyle: Theme.of(context)
-                      .textTheme
-                      .bodyText2
-                      ?.copyWith(fontSize: 20),
-                  hintText: 'Send a message...'),
+      color: Colors.transparent,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).backgroundColor,
+          borderRadius: BorderRadius.circular(100),
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 18),
+        child: Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                onChanged: onChanged,
+                controller: controller,
+                cursorColor: Theme.of(context).shadowColor,
+                maxLines: 1,
+                textAlignVertical: TextAlignVertical.center,
+                style: Theme.of(context).textTheme.bodyText1,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(borderSide: BorderSide.none),
+                    contentPadding: EdgeInsets.zero,
+                    hintStyle: Theme.of(context)
+                        .textTheme
+                        .bodyText2
+                        ?.copyWith(fontSize: 20),
+                    hintText: 'Send a message...'),
+              ),
             ),
-          )),
-          SizedBox(
-            width: 15,
-          ),
-          isTyping
-              ? GestureDetector(
-                  onTap: onSendClick,
-                  child: FaIcon(
-                    FontAwesomeIcons.solidArrowAltCircleRight,
-                    size: 25,
-                  ),
-                )
-              : GestureDetector(
-                  onTap: onAttachmentClick,
-                  child: FaIcon(
-                    FontAwesomeIcons.paperclip,
-                    size: 25,
-                  ),
-                )
-        ],
+            SizedBox(
+              width: 15,
+            ),
+            isTyping
+                ? GestureDetector(
+                    onTap: onSendClick,
+                    child: FaIcon(
+                      FontAwesomeIcons.solidArrowAltCircleRight,
+                      color: Theme.of(context).primaryColor,
+                      size: 30,
+                    ),
+                  )
+                : Row(
+                    children: [
+                      GestureDetector(
+                        onTap: onAttachmentClick,
+                        child: Icon(
+                          Icons.emoji_emotions,
+                          color: Theme.of(context).primaryColor,
+                          size: 25,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      GestureDetector(
+                        onTap: onAttachmentClick,
+                        child: FaIcon(
+                          FontAwesomeIcons.solidImage,
+                          color: Theme.of(context).primaryColor,
+                          size: 25,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      GestureDetector(
+                        onTap: onAttachmentClick,
+                        child: FaIcon(
+                          FontAwesomeIcons.paperclip,
+                          color: Theme.of(context).primaryColor,
+                          size: 22,
+                        ),
+                      ),
+                    ],
+                  )
+          ],
+        ),
       ),
     );
   }
