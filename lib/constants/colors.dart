3e7e9d72d7ab7a76392e1cf6ee1601088911e92e
Yuvraj Singh <ysgaur9919@gmail.com>
2021-10-24 19:16:46 +0530

    changed green shades

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,8 +2,8 @@ import 'package:flutter/widgets.dart';
 
 class DarkColors {
   static Color green1 = Color(0xff21AA68);
-  static Color green2 = Color(0xff0F9D58);
-  static Color green3 = Color(0xff2A6651);
+  static Color green2 = Color(0xff287154);
+  static Color green3 = Color(0xff2D4F49);
   static Color black1 = Color(0xff303841);
   static Color black2 = Color(0xff3A4750);
   static Color black3 = Color(0xff586675);
@@ -13,9 +13,9 @@ class DarkColors {
 }
 
 class LightColors {
-  static Color green1 = Color(0xff71EFA3);
-  static Color green2 = Color(0xff48DA82);
-  static Color green3 = Color(0xffD0F8E3);
+  static Color green1 = Color(0xff0adb76);
+  static Color green2 = Color(0xff7AE2B0);
+  static Color green3 = Color(0xffBCE7D4);
   static Color white1 = Color(0xffE9EAEB);
   static Color white2 = Color(0xffDBDCDC);
   static Color white3 = Color(0xff90a4b2);
